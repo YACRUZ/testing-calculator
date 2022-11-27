4b9a8774019238b8eeda3960e7c8ab9d264e5c32
YACRUZ <yamatsukino@gmail.com>
2022-11-27 14:08:35 -0600

    create division-ui

--- src/app/ui/ui.component.ts
+++ src/app/ui/ui.component.ts
@@ -3,6 +3,7 @@ import { addition } from "../addition/addition";
 import { substraction} from "../substraction/substraction";
 import { multiplication} from "../multiplication/multiplication";
 import { exp} from "../exp/exp";
+import { division} from "../division/division";
 
 @Component({
   selector: 'app-ui',
@@ -41,6 +42,12 @@ export class UiComponent implements OnInit {
   exp() {
     let myresult = 0;
     myresult = exp(this.operator1, this.operator2);
+    this.result = myresult;
+  }
+
+  division() {
+    let myresult = 0;
+    myresult = division(this.operator1, this.operator2);
     this.result = myresult;
   }
 
